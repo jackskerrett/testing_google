f26b39aef494084323770440f0a1ccab5d4859e6
Jack Skerrett <jackskerrett@gmail.com>
2021-09-29 21:17:39 +0100

    added in subber

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,21 @@
 from pytest import mark
-from src.app import adder
+from src.app import adder, subber
 
 @mark.parametrize("n1,n2,answer", [
     (3, 4, 7),
     (6, -1, 5),
-    (1999, 541, 2540)])
+    (1999, 541, 2540)
+])
+
 
 def test_adder(n1, n2, answer):
     assert adder(n1, n2) == answer
 
+@mark.parametrize("n1,n2,answer",[
+    (100,90,10),
+    (4,10,-6),
+    (5,5,0)
+])
 
-nxghmn
+def test_subber(n1, n2, answer):
+    assert subber(n1, n2) == answer  
