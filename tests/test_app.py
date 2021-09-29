79ac956c3aa1f617153cd4ffd191b4b491471385
Jack Skerrett <jackskerrett@gmail.com>
2021-09-29 20:48:25 +0100

    first tests

---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+from pytest import mark
+from src.app import adder
+
+@mark.parametrize("n1,n2,answer", [
+    (3, 4, 7),
+    (6, -1, 5),
+    (1999, 541, 2540)])
+
+def test_adder(n1, n2, answer):
+    assert adder(n1, n2) == answer
